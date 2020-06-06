72b1eb474741f795d8071e66e3305cb24c721a76
ionutmurgu11 <ionutmurgu11@yahoo.com>
2020-06-06 20:43:41 +0300

    adding lifecycle demo1
    Signed-off-by: ionutmurgu11 <ionutmurgu11@yahoo.com>

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Service;
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
-        return "Cats Are the Best!";
+        return "Cats Are the Best!!";
     }
 }
